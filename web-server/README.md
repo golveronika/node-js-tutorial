f804793ba6e381401693e7453a7cb56de50c583a
Вероника Голованова <veronika@MacBook-Pro-Veronika-2.local>
2020-05-09 18:11:30 +0200

    little fixings

---
+++ web-server/README.md
@@ -0,0 +1,3 @@
+## Usage: Weather App (web-server)
+1. Go to *web-server/src* folder
+2. Run `nodemon app.js`
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -87,7 +87,7 @@ app.get('*', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port 3000\nhttp://localhost:3000/')
 })
 
 // app.com
